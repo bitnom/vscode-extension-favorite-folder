26392c381d1233c013fdc05f14ea924089306f0f
bitnom <14287229+bitnom@users.noreply.github.com>
2025-11-27 23:40:24 -0500

    Refactor: Use native commands in context menu instead of wrappers to fix crash

--- src/extension.ts
+++ src/extension.ts
@@ -333,52 +333,6 @@ export function activate(context: vscode.ExtensionContext) {
 		})
 	);
 
-	// Wrapper commands for standard file actions
-	context.subscriptions.push(
-		vscode.commands.registerCommand('favorite-folders.revealInExplorer', async (item: FavoriteFolder) => {
-			const uri = item.resourceUri || item.uri;
-			if (uri) {
-				await vscode.commands.executeCommand('revealInExplorer', uri);
-			}
-		})
-	);
-
-	context.subscriptions.push(
-		vscode.commands.registerCommand('favorite-folders.openInTerminal', async (item: FavoriteFolder) => {
-			const uri = item.resourceUri || item.uri;
-			if (uri) {
-				await vscode.commands.executeCommand('openInTerminal', uri);
-			}
-		})
-	);
-
-	context.subscriptions.push(
-		vscode.commands.registerCommand('favorite-folders.findInFolder', async (item: FavoriteFolder) => {
-			const uri = item.resourceUri || item.uri;
-			if (uri) {
-				await vscode.commands.executeCommand('filesExplorer.findInFolder', uri);
-			}
-		})
-	);
-
-	context.subscriptions.push(
-		vscode.commands.registerCommand('favorite-folders.copyPath', async (item: FavoriteFolder) => {
-			const uri = item.resourceUri || item.uri;
-			if (uri) {
-				await vscode.commands.executeCommand('copyFilePath', uri);
-			}
-		})
-	);
-
-	context.subscriptions.push(
-		vscode.commands.registerCommand('favorite-folders.copyRelativePath', async (item: FavoriteFolder) => {
-			const uri = item.resourceUri || item.uri;
-			if (uri) {
-				await vscode.commands.executeCommand('copyRelativeFilePath', uri);
-			}
-		})
-	);
-
 	context.subscriptions.push(
 		vscode.commands.registerCommand('favorite-folders.createVirtualFolder', async (parent?: FavoriteFolder) => {
 			const name = await vscode.window.showInputBox({ prompt: 'Enter Virtual Folder Name' });
